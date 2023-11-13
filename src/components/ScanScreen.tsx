d84621751e096710f043b3d0293a863037e9b586
Andy Phillipson <ajp@mindspring.com>
2023-11-13 03:07:35 -0500

    Add basic camera support for scan

--- src/components/ScanScreen.tsx
+++ src/components/ScanScreen.tsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { ScannerView } from '@react-native-ajp-elements/ui';
 import { useTheme } from 'theme';
 
 const ScanScreen = () => {
   const theme = useTheme();
 
+  const onScan = (data: string) => {
+    console.log(`scan data ${data}`);
+  };
+
   return (
     <SafeAreaView
       edges={['left', 'right']}
       style={[theme.styles.view, { paddingHorizontal: 0 }]}
-    />
+    >
+      <ScannerView onScan={onScan}/>
+    </SafeAreaView>
   );
 };
 

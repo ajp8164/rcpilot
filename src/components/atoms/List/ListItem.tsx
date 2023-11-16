67f9478507a00996e20cf923c0a1ae72cf0c9dbe
Andy Phillipson <ajp@mindspring.com>
2023-11-16 15:23:46 -0500

    Add expandable section to list item

--- src/components/atoms/List/ListItem.tsx
+++ src/components/atoms/List/ListItem.tsx
@@ -1,27 +1,53 @@
 import { AppTheme, useTheme } from "theme";
 
+import { ExpandableSection } from "react-native-ui-lib";
 import { ListItem as _ListItem } from "@react-native-ajp-elements/ui";
 import { makeStyles } from "@rneui/themed";
 
-interface Props extends _ListItem {};
+interface Props extends _ListItem {
+  expanded?: boolean;
+  ExpandableComponent?: JSX.Element;
+};
 
 const ListItem = (props: Props) => {
+  const { 
+    expanded = false,
+    ExpandableComponent,
+  } = props;
+
   const theme = useTheme();
   const s = useStyles(theme);
   return (
+    <>
     <_ListItem
-      {...props} 
-      containerStyle={{...props.containerStyle, ...s.containerStyle}}
-      valueStyle={s.valueStyle} />
-    );
+      {...props}
+      containerStyle={{...props.containerStyle, ...s.container}}
+      valueStyle={[
+        {...props.valueStyle, ...s.value},
+        props.disabled ? s.valuePosition : {},
+        !props.rightImage && props.value ? s.valuePosition : {}
+      ]}
+      disabledStyle={s.disabled}
+      rightImage={props.disabled === undefined ? props.rightImage : !props.disabled}
+    />
+    <ExpandableSection expanded={expanded}>
+      {ExpandableComponent}
+    </ExpandableSection>
+  </>);
 }
 
 const useStyles = makeStyles((_theme, theme: AppTheme) => ({
-  containerStyle: {
+  container: {
     minHeight: 48
   },
-  valueStyle: {
+  disabled: {
+    backgroundColor: theme.colors.listItem
+  },
+  value: {
     ...theme.styles.textDim
+  },
+  valuePosition: {
+    paddingRight: 18,
   }
 }));
 

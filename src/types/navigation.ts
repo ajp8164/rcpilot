9cdf5ddbe6b57b8a3e391a1555ff401be385a301
Andy Phillipson <ajp@mindspring.com>
2023-11-17 13:16:09 -0500

    Add filters for batteries screen

--- src/types/navigation.ts
+++ src/types/navigation.ts
@@ -24,6 +24,7 @@ export type BatteriesNavigatorParamList = {
   BatteryCycle: {
     batteryCycleId: string;
   };
+  BatteryFilters: undefined;
   BatteryPerformance: undefined;
   BatteryTint: undefined;
   NewBatteryNavigator: undefined;

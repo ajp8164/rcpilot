a18d14a6cd64fd1702fc8616ec8dc227d544c4a6
Andy Phillipson <ajp@mindspring.com>
2023-11-17 16:36:56 -0500

    Enable list item switch with expandable section

--- src/components/atoms/List/ListItemSwitch.tsx
+++ src/components/atoms/List/ListItemSwitch.tsx
@@ -1,18 +1,32 @@
 import { AppTheme, useTheme } from "theme";
 
+import { ExpandableSection } from "react-native-ui-lib";
 import { ListItemSwitch as _ListItemSwitch } from "@react-native-ajp-elements/ui";
 import { makeStyles } from "@rneui/themed";
 
-interface Props extends _ListItemSwitch {};
+interface Props extends _ListItemSwitch {
+  expanded?: boolean;
+  ExpandableComponent?: JSX.Element;
+};
 
 const ListItemSwitch = (props: Props) => {
+  const { 
+    expanded = false,
+    ExpandableComponent,
+  } = props;
+
   const theme = useTheme();
   const s = useStyles(theme);
   return (
-    <_ListItemSwitch
-      {...props}
-      containerStyle={{...props.containerStyle, ...s.containerStyle}}
-    />
+    <>
+      <_ListItemSwitch
+        {...props}
+        containerStyle={{...props.containerStyle, ...s.containerStyle}}
+      />
+      <ExpandableSection expanded={expanded}>
+        {ExpandableComponent}
+      </ExpandableSection>
+    </>
   );
 }
 

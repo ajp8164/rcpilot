fc15f5db5555c29ba74c89bafacf15693f283cf5
Andy Phillipson <ajp@mindspring.com>
2023-11-25 01:45:07 -0500

    Remove obsolete chemistries global

--- src/lib/battery/chemistries.ts
+++
@@ -1,9 +0,0 @@
-export const chemistries = [
-  { name: 'LiFe', icon: '' },
-  { name: 'LiHV', icon: '' },
-  { name: 'LiIon', icon: '' },
-  { name: 'LiPo', icon: '' },
-  { name: 'MiCd', icon: '' },
-  { name: 'NNiMH', icon: '' },
-  { name: 'Other', icon: '' },
-];

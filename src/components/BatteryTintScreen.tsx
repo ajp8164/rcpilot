29d91a2b67c82abdd6c5741b70fca65ffabd7edb
Andy Phillipson <ajp@mindspring.com>
2023-11-24 20:29:31 -0500

    Remove unused navigation props

--- src/components/BatteryTintScreen.tsx
+++ src/components/BatteryTintScreen.tsx
@@ -1,13 +1,9 @@
 import { BatteryTint } from 'types/battery';
 import { Divider } from '@react-native-ajp-elements/ui';
 import { ListItemCheckbox } from 'components/atoms/List';
-import { NativeStackScreenProps } from '@react-navigation/native-stack';
-import { NewBatteryNavigatorParamList } from 'types/navigation';
 import React from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { useTheme } from 'theme';
-
-export type Props = NativeStackScreenProps<NewBatteryNavigatorParamList, 'BatteryTint'>;
 
 const BatteryTintScreen = () => {
   const theme = useTheme();

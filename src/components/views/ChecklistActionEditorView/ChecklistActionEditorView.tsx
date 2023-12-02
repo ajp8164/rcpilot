16c89179a1e6040fdad2aab0621d0700fbc74723
Andy Phillipson <ajp@mindspring.com>
2023-12-01 22:54:04 -0500

    Add wheel picker to checklist action schedule ui

--- src/components/views/ChecklistActionEditorView/ChecklistActionEditorView.tsx
+++ src/components/views/ChecklistActionEditorView/ChecklistActionEditorView.tsx
@@ -6,6 +6,8 @@ import React, { useState } from 'react';
 
 import { Divider } from '@react-native-ajp-elements/ui';
 import { View } from 'react-native';
+import WheelPicker from 'components/atoms/WheelPicker';
+import { getChecklistActionScheduleItems } from 'lib/checklistTemplate';
 import { makeStyles } from '@rneui/themed';
 import { useNavigation } from '@react-navigation/core';
 
@@ -29,9 +31,15 @@ const ChecklistActionEditor = (props: ChecklistActionEditorViewProps) => {
   };
 
   const [actionRepeatsEnabled, setActionRepeatsEnabled] = useState(false);
+  const [schedulePickerOpen, setSchedulePickerOpen] = useState(false);
+  const [actionSchedule, setActionSchedule] = useState<string[]>();
 
   const toggleActionRepeats = (value: boolean) => {
     setActionRepeatsEnabled(value);
+  };
+
+  const toggleSchedulePickerOpen = () => {
+    setSchedulePickerOpen(!schedulePickerOpen);
   };
 
   return (
@@ -48,8 +56,24 @@ const ChecklistActionEditor = (props: ChecklistActionEditorViewProps) => {
         title={'Perform'}
         value={'Today'}
         position={['first']}
-        onPress={() => null}
+        onPress={toggleSchedulePickerOpen}
+        expanded={schedulePickerOpen}
+        ExpandableComponent={
+          <WheelPicker
+            placeholder={'none'}
+            itemWidth={['30%', '70%']}
+            items={getChecklistActionScheduleItems(action).items}
+            value={actionSchedule || getChecklistActionScheduleItems(action).defaultValue}
+            onValueChange={(_wheelIndex, value, _index) => {
+              setActionSchedule(value as string[]);
+            }}
+          />
+        }
       /> 
+      <ListItem
+        title={'Following'}
+        value={'Today'}
+      />
       <ListItemSwitch
         title={'Action Repeats'}
         value={actionRepeatsEnabled}

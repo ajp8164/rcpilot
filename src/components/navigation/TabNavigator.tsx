60e3353e1a3425d2cc73e111369f64077893a1c3
Andy Phillipson <ajp@mindspring.com>
2023-12-21 13:00:19 -0500

    Log tab icon

--- src/components/navigation/TabNavigator.tsx
+++ src/components/navigation/TabNavigator.tsx
@@ -56,6 +56,7 @@ const TabNavigator = () => {
           // eslint-disable-next-line react/no-unstable-nested-components
           tabBarIcon: ({ color }) => (
             <Icon
+              solid
               name={'file-lines'}
               color={color}
               size={28}

263cb5cdc5b1fab2052738c051f9f1e6a927725e
Andy Phillipson <ajp@mindspring.com>
2023-12-31 17:16:48 -0500

    Bind Pilots screen to realm

--- src/components/PilotsScreen.tsx
+++ src/components/PilotsScreen.tsx
@@ -1,16 +1,17 @@
 import { AppTheme, useTheme } from 'theme';
 import { FlatList, ListRenderItem } from 'react-native';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 
 import { Button } from '@rneui/base';
 import { Divider } from '@react-native-ajp-elements/ui';
 import Icon from 'react-native-vector-icons/FontAwesome6';
 import { ListItemCheckboxInfo } from 'components/atoms/List';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
-import { Pilot } from 'types/pilot';
+import { Pilot } from 'realmdb/Pilot';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { SetupNavigatorParamList } from 'types/navigation';
 import { makeStyles } from '@rneui/themed';
+import { useQuery } from '@realm/react';
 
 export type Props = NativeStackScreenProps<SetupNavigatorParamList, 'Pilots'>;
 
@@ -18,16 +19,7 @@ const PilotsScreen = ({ navigation }: Props) => {
   const theme = useTheme();
   const s = useStyles(theme);
 
-  const [items, setItems] = useState<Pilot[]>([
-    {
-      id: '1',
-      name: 'Andy',
-    },
-    {
-      id: '2',
-      name: 'Lisa',
-    },
-  ]);
+  const allPilots = useQuery(Pilot);
 
   useEffect(() => {
     navigation.setOptions({
@@ -46,37 +38,44 @@ const PilotsScreen = ({ navigation }: Props) => {
   const renderItems: ListRenderItem<Pilot> = ({ item, index }) => {
     return (
       <ListItemCheckboxInfo
-        key={item.id}
+      key={item._id.toString()}
         title={item.name}
-        position={items.length === 1 ? ['first', 'last'] : index === 0 ? ['first'] : index === items.length - 1 ? ['last'] : []}
+        position={allPilots.length === 1 ? ['first', 'last'] : index === 0 ? ['first'] : index === allPilots.length - 1 ? ['last'] : []}
         checked={true}
         onPress={() => null}
         onPressInfo={() => navigation.navigate('Pilot')}
       />
     )
-};
+  };
+
+  const renderFooter = () => {
+    return (
+      <>
+        <ListItemCheckboxInfo
+          title={'Unknown Pilot'}
+          subtitle={'Logged 0:04 over 1 event'}
+          position={['first', 'last']}
+          hideInfo={true}
+          checked={true}
+          onPress={() => null}
+        />
+        <Divider type={'note'} text={'Includes events logged with an "Unknown" pilot and model time not directly associated with an event.'}  />
+      </>
+    );
+  };
 
   return (
     <SafeAreaView
       edges={['left', 'right']}
       style={theme.styles.view}>
-      <Divider />
       <FlatList
-        data={items}
+        data={allPilots}
         renderItem={renderItems}
-        keyExtractor={(_item, index) => `${index}`}
+        keyExtractor={item => item._id.toString()}
         showsVerticalScrollIndicator={false}
+        ListHeaderComponent={Divider}
+        ListFooterComponent={renderFooter}
       />
-      <Divider />
-      <ListItemCheckboxInfo
-        title={'Unknown Pilot'}
-        subtitle={'Logged 0:04 over 1 event'}
-        position={['first', 'last']}
-        hideInfo={true}
-        checked={true}
-        onPress={() => null}
-      />
-      <Divider type={'note'} text={'Includes events logged with an "Unknown" pilot and model time not directly associated with an event.'}  />
     </SafeAreaView>
   );
 };

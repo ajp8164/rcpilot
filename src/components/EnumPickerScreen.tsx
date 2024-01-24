ded68be33ad432bf87142953bce75ea2769dfd26
Andy Phillipson <ajp@mindspring.com>
2024-01-24 11:04:24 -0500

    Fix selection handling and button state reponse

--- src/components/EnumPickerScreen.tsx
+++ src/components/EnumPickerScreen.tsx
@@ -2,6 +2,7 @@ import { AppTheme, useTheme } from 'theme';
 import { FlatList, ListRenderItem, ScrollView, View } from 'react-native';
 import { ListItem, ListItemCheckbox } from 'components/atoms/List';
 import React, { useEffect } from 'react';
+import lodash, { isArray } from 'lodash';
 
 import { Button } from '@rneui/base';
 import { Divider } from '@react-native-ajp-elements/ui';
@@ -9,7 +10,6 @@ import Icon from 'react-native-vector-icons/FontAwesome6';
 import { MultipleNavigatorParamList } from 'types/navigation';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { isArray } from 'lodash';
 import { makeStyles } from '@rneui/themed';
 import { useEvent } from 'lib/event';
 import { useSetState } from '@react-native-ajp-elements/core';
@@ -49,18 +49,32 @@ const EnumPickerScreen = ({ route,  navigation }: Props) => {
     selected,
     eventName,
   } = route.params;
-
   const theme = useTheme();
   const s = useStyles(theme);
 
   const event = useEvent();
 
-  const [list, setList] = useSetState<{ values: string[]; selected: string[]; }>({
+  const [list, setList] = useSetState<{ values: string[]; selected: string[]; initial: string[]; }>({
     values,
-    selected: isArray(selected) ? selected : [selected],
+     // Use an empty array if empty string is set.
+    selected: isArray(selected) ? selected : selected ? [selected] : [],
+    initial: isArray(selected) ? selected : selected ? [selected] : [],
   });
 
   useEffect(() => {
+    const canSave = () => {
+      // Check if arrays contain the same elements.
+      return (!lodash.isEmpty(lodash.xor(list.selected, list.initial)));
+    };
+
+    const onDone = () => {
+      // For multi-selection mode we send the selected values only when done.
+      if (mode === 'many' || mode === 'many-or-none') {
+        event.emit(eventName, list.selected);
+        navigation.goBack();
+      }
+    };
+  
     navigation.setOptions({
       title,
       headerBackTitle,
@@ -79,27 +93,29 @@ const EnumPickerScreen = ({ route,  navigation }: Props) => {
           )
         },
         headerRight: ()  => {
-          return (
-            <Button
-              title={'Done'}
-              titleStyle={theme.styles.buttonClearTitle}
-              buttonStyle={[theme.styles.buttonClear, s.doneButton]}
-              onPress={onDone}
-            />
-          )
+          if (canSave()) {
+            return (
+              <Button
+                title={'Done'}
+                titleStyle={theme.styles.buttonClearTitle}
+                buttonStyle={[theme.styles.buttonClear, s.doneButton]}
+                onPress={onDone}
+              />
+            )
+          };
         },
       });
     }
-  }, []);
+  }, [ list ]);
 
   const toggleSelect = (value: string) => {
     if (mode === 'one') {
       setList({ selected: [value] });
     } else {
       if (list.selected.includes(value)) {
-        setList({ selected: list.selected.filter(v => v !== value) });
+        setList({ selected: list.selected.filter(v => v !== value) }, {assign: true});
       } else {
-        setList({ selected: list.selected.concat(value) });
+        setList({ selected: list.selected.concat(value) }, {assign: true});
       }
     }
 
@@ -119,13 +135,6 @@ const EnumPickerScreen = ({ route,  navigation }: Props) => {
 
   const selectUnspecified = () => {
     setList({ selected: ['Unspecified'] }, { assign: true });
-  };
-
-  const onDone = () => {
-    // For multi-selection mode we send the selected values only when done.
-    if (mode === 'many' || mode === 'many-or-none') {
-      event.emit(eventName, list.selected);
-    }
   };
 
   const getIconEl = (value: string) => {
@@ -150,7 +159,7 @@ const EnumPickerScreen = ({ route,  navigation }: Props) => {
     return iconArr ? <View style={{flexDirection: 'row'}}>{iconArr}</View> : undefined;
   };
 
-  const renderValues: ListRenderItem<string> = ({ item: value, index }) => {
+  const renderValue: ListRenderItem<string> = ({ item: value, index }) => {
     return (
       <ListItemCheckbox
         key={`${value}${index}`}
@@ -190,7 +199,7 @@ const EnumPickerScreen = ({ route,  navigation }: Props) => {
         <Divider text={sectionName} />
         <FlatList
           data={list.values}
-          renderItem={renderValues}
+          renderItem={renderValue}
           keyExtractor={(_item, index) => `${index}`}
           showsVerticalScrollIndicator={false}
           scrollEnabled={false}

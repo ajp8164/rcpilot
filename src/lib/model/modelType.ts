f8c43b07c5e23e9a3adb0312bea55183830742d0
Andy Phillipson <ajp@mindspring.com>
2024-02-06 10:19:14 -0500

    Separate entries for mode type icons

--- src/lib/model/modelType.ts
+++ src/lib/model/modelType.ts
@@ -1,18 +1,12 @@
 import { EnumPickerIconProps } from 'components/EnumPickerScreen';
 import { ModelType } from 'types/model';
 
-const icon: EnumPickerIconProps = {
-  name: 'plane-up',
-  size: 28,
-  style: { width: 35 },
-};
-
 export const modelTypeIcons: {[key in ModelType]: EnumPickerIconProps} = {
-  [ModelType.Airplane]: {...icon, color: 'blue'},
-  [ModelType.Sailplane]: {...icon, color: 'blue'},
-  [ModelType.Helicopter]: {...icon, color: 'blue'},
-  [ModelType.Multicopter]: {...icon, color: 'blue'},
-  [ModelType.Car]: {...icon, color: 'blue'},
-  [ModelType.Boat]: {...icon, color: 'blue'},
-  [ModelType.Robot]: {...icon, color: 'blue'},
+  [ModelType.Airplane]: {name: 'plane-up', size: 28, style: { width: 35 }, color: 'blue'},
+  [ModelType.Sailplane]: {name: 'plane-up', size: 28, style: { width: 35 }, color: 'blue'},
+  [ModelType.Helicopter]: {name: 'plane-up', size: 28, style: { width: 35 }, color: 'blue'},
+  [ModelType.Multicopter]: {name: 'plane-up', size: 28, style: { width: 35 }, color: 'blue'},
+  [ModelType.Car]: {name: 'plane-up', size: 28, style: { width: 35 }, color: 'blue'},
+  [ModelType.Boat]: {name: 'plane-up', size: 28, style: { width: 35 }, color: 'blue'},
+  [ModelType.Robot]: {name: 'plane-up', size: 28, style: { width: 35 }, color: 'blue'},
 }

a248d1e57fdf526089a679daa94fd83c2d8621c0
Andy Phillipson <ajp@mindspring.com>
2024-02-07 12:49:21 -0500

    Animated model header

--- src/components/molecules/ModelHeader.tsx
+++ src/components/molecules/ModelHeader.tsx
@@ -1,5 +1,5 @@
 import { AppTheme, useTheme } from 'theme';
-import { Image, Pressable, View } from 'react-native';
+import { Image, Platform, Pressable, Text, View } from 'react-native';
 import React, { useState } from 'react';
 
 import { BSON } from 'realm';
@@ -7,25 +7,68 @@ import CircleButton from 'components/atoms/CircleButton';
 import Icon from 'react-native-vector-icons/FontAwesome6';
 import { Model } from 'realmdb/Model';
 import { makeStyles } from '@rneui/themed';
-import { selectImage } from '@react-native-ajp-elements/ui';
+import { getColoredSvg, selectImage } from '@react-native-ajp-elements/ui';
 import { useObject } from '@realm/react';
+import { SvgXml } from 'react-native-svg';
+import { modelTypeIcons } from 'lib/model';
+import Animated, { Extrapolation, SharedValue, interpolate, useAnimatedStyle } from 'react-native-reanimated';
 
 interface ModelHeaderInterface {
   modelId: string;
   onChangeImage?: (image?: string) => void;
   onGoBack?: () => void;
+  scrollY?: SharedValue<number>;
 }
 
 export const ModelHeader = ({
   modelId,
   onChangeImage,
   onGoBack,
+  scrollY,
 }: ModelHeaderInterface) => {
   const theme = useTheme();
   const s = useStyles(theme);
 
   const model = useObject(Model, new BSON.ObjectId(modelId));
   const [image, setImage] = useState(model?.image || undefined);
+
+  const minHeight = theme.insets.top + 39;
+  const maxHeight = 150;
+
+  const backgroundOpacity = useAnimatedStyle(() => {
+    if (!scrollY) return {};
+    return {
+      opacity: interpolate(scrollY.value, [0, 50], [1, 0], Extrapolation.CLAMP),
+    };
+  });
+
+  const backgroundTranslateY = useAnimatedStyle(() => {
+    if (!scrollY) return {};
+    return {
+      height: interpolate(scrollY.value, [0, 40], [maxHeight, minHeight], Extrapolation.CLAMP),
+    };
+  });
+
+  const itemsOpacity = useAnimatedStyle(() => {
+    if (!scrollY) return {};
+    return {
+      opacity: interpolate(scrollY.value, [0, 18], [1, 0], Extrapolation.CLAMP),
+    };
+  });
+
+  const itemsTranslateY = useAnimatedStyle(() => {
+    if (!scrollY) return {};
+    return {
+      transform: [{translateY: interpolate(scrollY.value, [0, maxHeight], [0, -300], Extrapolation.CLAMP)}],
+    };
+  });
+
+  const collapsedHeaderOpacity = useAnimatedStyle(() => {
+    if (!scrollY) return {};
+    return {
+      opacity: interpolate(scrollY.value, [30, 50], [0, 1], Extrapolation.CLAMP),
+    };
+  });
 
   const selectModelImage = () => {
     selectImage({
@@ -38,53 +81,133 @@ export const ModelHeader = ({
   };
 
   return (
-    <View style={s.headerContainer}>
-      <Image
-        source={{ uri: image }}
-        resizeMode={'cover'}
-        style={s.headerImage}
-      />
-      <View style={s.headerButtonRightContainer}>
-        <CircleButton
-          size={30}
-          icon={'camera'}
-          onPress={selectModelImage}
+    <>
+      {/* // Collapsed header */}
+      <Animated.View style={[s.collapsedHeader, {height: minHeight}, collapsedHeaderOpacity]}>
+          <Text style={s.title}>
+            {model?.name}
+          </Text>
+      </Animated.View>
+      {/* Background image */}
+      <Animated.View
+        style={[s.backgroundContainer, backgroundTranslateY, backgroundOpacity]}>
+        <Image
+          source={{ uri: image }}
+          resizeMode={'cover'}
+          style={s.headerImage}
         />
-      </View>
-      {onGoBack && <Pressable
-        onPress={onGoBack}
-        style={s.headerButtonLeftContainer}>
-        <Icon
-          name={'chevron-left'}
-          style={s.buttonLeft}
-        />
-      </Pressable>}
-    </View>
+      </Animated.View>
+      {/* Left button */}
+      {onGoBack &&
+        <Pressable
+          onPress={onGoBack}
+          style={s.buttonLeftContainer}>
+          <Animated.View style={collapsedHeaderOpacity}>
+            <Icon name={'chevron-left'} style={[s.buttonLeft, s.buttonLeftCollapsed]} />
+          </Animated.View>
+          <Animated.View style={backgroundOpacity}>
+            <Icon name={'chevron-left'} style={[s.buttonLeft]} />
+          </Animated.View>
+        </Pressable>
+      }
+      {/* Items */}
+      <Animated.View
+        style={[s.itemsContainer, itemsTranslateY, itemsOpacity]}>
+        <View style={s.buttonRightContainer}>
+          <CircleButton
+            size={30}
+            icon={'camera'}
+            onPress={() => (!model || (scrollY && scrollY.value < 5)) && selectModelImage()}
+          />
+        </View>
+        {model &&
+          <View style={s.insetImageContainer}>
+            <SvgXml
+              xml={getColoredSvg(modelTypeIcons[model.type]?.name as string)}
+              width={75}
+              height={75}
+              color={theme.colors.stickyWhite}
+              style={[s.insetImage]}
+            />
+          </View>
+        }
+      </Animated.View>
+    </>
   );
 };
 
 const useStyles = makeStyles((_theme, theme: AppTheme) => ({
-  headerContainer: {
-    overflow: 'visible',
-    width: '100%',
+  backgroundContainer: {
     height: 150,
     backgroundColor: theme.colors.lightGray,
   },
-  headerImage: {
-    flex: 1,
+  buttonLeft: {
+    top: -22,
+    fontSize: 22,
+    color: theme.colors.whiteTransparentMid,
   },
-  headerButtonRightContainer: {
-    position: 'absolute',
-    right: 15,
-    top: theme.insets.top,
-  },
-  headerButtonLeftContainer: {
+  buttonLeftContainer: {
     position: 'absolute',
     left: 8,
     top: theme.insets.top + 5,
   },
-  buttonLeft: {
-    fontSize: 22,
-    color: theme.colors.whiteTransparentMid,
+  buttonRightContainer: {
+    position: 'absolute',
+    right: 15,
+    top: theme.insets.top,
+    alignItems: 'flex-end',
+  },
+  buttonLeftCollapsed: {
+    top: 0,
+    color: theme.colors.screenHeaderBackButton,
+  },
+  collapsedHeader: {
+    backgroundColor: theme.colors.white,
+    position: 'absolute',
+    width: '100%',
+    alignItems: 'center', 
+    shadowColor: theme.colors.stickyBlack,
+    ...Platform.select({
+      ios: {
+        shadowOffset: {
+          width: 0,
+          height: 0.35,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 0.35,
+      },
+      android: {
+        elevation: 5,
+        backgroundColor: theme.colors.black,
+      },
+    }),
+  },
+  title: {
+    position: 'absolute',
+    bottom: 12,
+    fontSize: 17,
+    fontWeight: '600'
+  },
+  headerImage: {
+    flex: 1,
+  },
+  itemsContainer: {
+    position: 'absolute',
+    width: '100%',
+  },
+  insetImage: {
+    transform: [{rotate: '-45deg'}],
+    left: -3,
+    top: -1,    
+  },
+  insetImageContainer: {
+    width: 70,
+    height: 70,
+    left: 35,
+    top: 110,
+    borderWidth: 3,
+    borderRadius: 10,
+    borderColor: theme.colors.viewBackground,
+    backgroundColor: theme.colors.darkGray,
   },
 }));

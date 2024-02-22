a8f35cf8760090e0ab6d6a8a7f83a4676f4a993d
Andy Phillipson <ajp@mindspring.com>
2024-02-22 08:35:08 -0500

    Formatter minor fix

--- src/lib/formatters.ts
+++ src/lib/formatters.ts
@@ -1,8 +1,7 @@
-// Masks the given number directly into the format 'hh:mm:ss'. Fewer than
-// 6 numeric digits are padded with '0'.
-
 import { Duration } from "luxon";
 
+// Masks the given number directly into the format 'hh:mm:ss'. Fewer than
+// 6 numeric digits are padded with '0'.
 // Example, 123456 is output as '12:34:56'.
 export const maskToHMS = (value: number) => {
   return value.toString().padStart(6, '0').split(/(?=(?:..)*$)/).join(':');
@@ -28,5 +27,5 @@ export const MSSToSeconds = (value: number | string) => {
 export const secondsToMSS = (value?: number | string) => {
   let v = typeof value === 'string' ? parseFloat(value) : value;
   const duration = Duration.fromObject({ seconds: v });
-  return duration.toFormat('m.ss');
+  return duration.toFormat('m:ss');
 };

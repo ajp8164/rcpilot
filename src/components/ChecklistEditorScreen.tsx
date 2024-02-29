fa2eeb2effb7b56ca521ee43c61ab72fdb2a690f
Andy Phillipson <ajp@mindspring.com>
2024-02-29 14:08:28 -0500

    Remove console logs

--- src/components/ChecklistEditorScreen.tsx
+++ src/components/ChecklistEditorScreen.tsx
@@ -187,7 +187,6 @@ const ChecklistEditorScreen = ({ navigation, route }: Props) => {
   };
 
   const upsertAction = (newOrChangedAction: JChecklistAction) => {
-    console.log(JSON.stringify(newOrChangedAction));
     if (newOrChangedAction.refId !== undefined) {
       // Update existing action.
       setActions(prevState => {

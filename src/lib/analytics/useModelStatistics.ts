85fc25270b69d459cbc42c9cf9bf1a14c9646a72
Andy Phillipson <ajp@mindspring.com>
2024-03-06 10:15:18 -0500

    Debug event style statistics

--- src/lib/analytics/useModelStatistics.ts
+++ src/lib/analytics/useModelStatistics.ts
@@ -4,6 +4,7 @@ import { EventOutcome } from 'types/event';
 import { EventStyle } from 'realmdb/EventStyle';
 import { useRealm } from '@realm/react';
 
+export type ModelStatisticsMode = 'add' | 'update';
 export type ModelCostStatistics = {
   perEventCost: number;
   totalMaintenanceCost: number;
@@ -16,10 +17,12 @@ export const useModelStatistics = () => {
   const realm = useRealm();
 
   return (
+    mode: ModelStatisticsMode,
     model: Model,
-    theEventDuration: number,
-    theEventStyle?: EventStyle,
-    theEventOutcome?: EventOutcome,
+    newEventDuration: number,
+    newEventOutcome?: EventOutcome,
+    oldEventStyle?: EventStyle,
+    newEventStyle?: EventStyle,
   ) => {
     // Event duration data.
     const eventDurationData = [] as ModelEventDurationData[];
@@ -29,18 +32,25 @@ export const useModelStatistics = () => {
     if (allEventStyles.length) {
       allEventStyles.forEach(style => {
         eventDurationData.push(
-          computeEventDurationData(model, theEventDuration, theEventStyle, style)
+          computeEventDurationData(
+            mode,
+            model,
+            newEventDuration,
+            oldEventStyle,
+            newEventStyle,
+            style,
+          )
         );
       });
     }
     // Unspecified style.
     eventDurationData.push(
-      computeEventDurationData(model, theEventDuration, theEventStyle)
+      computeEventDurationData(mode, model, newEventDuration, oldEventStyle, newEventStyle)
     );
 
     // Crash summary.
     let crashCount = model!.statistics.crashCount;
-    if (theEventOutcome === EventOutcome.Crashed) {
+    if (newEventOutcome === EventOutcome.Crashed) {
       crashCount++;
     }
 
@@ -93,13 +103,15 @@ export const updateModelMaintenanceCost = (model: Model, oldValue: number, newVa
 // style to account for 'unspecified' event style. Duration data entries are lazily created to
 // ensure all statisitics match user defined event styles.
 const computeEventDurationData = (
+  mode: ModelStatisticsMode,
   model: Model,
-  theEventDuration: number,
-  theEventStyle?: EventStyle,
+  newEventDuration: number,
+  oldEventStyle?: EventStyle,
+  newEventStyle?: EventStyle,
   style?: EventStyle,
 ) => {
   // Find existing duration data for the specified style.
-  let previousDurationData = model?.statistics.eventDurationData.find(d =>
+  let previousDurationData = model.statistics.eventDurationData.find(d =>
     d.eventStyleId === (style?._id.toString() || '')
   );
 
@@ -118,12 +130,20 @@ const computeEventDurationData = (
     eventStyleDuration: previousDurationData.eventStyleDuration,
   };
 
-  if ((style?._id.toString() || undefined) === (theEventStyle?._id.toString() || undefined)) {
-    // Update data using this event.
+  // Update data using this event.
+  // Add to the new event style, remove from the old event style.
+  // The mode tells us if we should ever reduce stats in an event style.
+  if (style?._id.toString() === newEventStyle?._id.toString()) {
     durationData = {
       ...previousDurationData,
       eventStyleCount: previousDurationData.eventStyleCount + 1,
-      eventStyleDuration: previousDurationData.eventStyleDuration + theEventDuration,
+      eventStyleDuration: previousDurationData.eventStyleDuration + newEventDuration,
+    } as ModelEventDurationData;
+  } else if (mode === 'update' && style?._id.toString() === oldEventStyle?._id.toString()) {
+    durationData = {
+      ...previousDurationData,
+      eventStyleCount: previousDurationData.eventStyleCount - 1,
+      eventStyleDuration: previousDurationData.eventStyleDuration - newEventDuration,
     } as ModelEventDurationData;
   }
 

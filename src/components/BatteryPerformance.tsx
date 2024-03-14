72dcfddb27e0f26f393ab252ccca3c1fc92ad679
Andy Phillipson <ajp@mindspring.com>
2024-03-14 09:09:32 -0400

    Use events filter in place of battery performance filter, delete battery performance filter

--- src/components/BatteryPerformance.tsx
+++ src/components/BatteryPerformance.tsx
@@ -1,11 +1,10 @@
-import { Text, View } from 'react-native';
-
 import ActionBar from 'components/atoms/ActionBar';
 import { BatteriesNavigatorParamList } from 'types/navigation';
 import { EmptyView } from 'components/molecules/EmptyView';
 import Icon from 'react-native-vector-icons/FontAwesome6';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import React from 'react';
+import { View } from 'react-native';
 import { useTheme } from 'theme';
 
 export type Props = NativeStackScreenProps<BatteriesNavigatorParamList, 'BatteryPerformance'>;
@@ -19,10 +18,25 @@ const BatteryPerformanceScreen = ({ navigation }: Props) => {
       <ActionBar
         actions={[
           {
-            ActionComponent: (<Icon name={'filter'} size={28} color={theme.colors.brandPrimary} />),
-            onPress: () => navigation.navigate('BatteryPerformanceNavigator')
+            ActionComponent: (
+              <Icon
+                name={'filter'}
+                size={28}
+                color={theme.colors.clearButtonText}
+              />
+            ),
+            onPress: () => navigation.navigate('EventFiltersNavigator', {
+              screen: 'EventFilters',
+              params: {},
+            })
           }, {
-            ActionComponent: (<Icon name={'scale-unbalanced-flip'} size={28} color={theme.colors.brandPrimary} />),
+            ActionComponent: (
+              <Icon
+                name={'scale-unbalanced-flip'}
+                size={28}
+                color={theme.colors.clearButtonText}
+              />
+            ),
             onPress: () => navigation.navigate('BatteryPerformanceComparisonPicker')
           }, {
             label: 'Done',

3f551d84109c12021583fa86732e69dd6cb1448e
Andy Phillipson <ajp@mindspring.com>
2024-03-17 20:37:34 -0400

    Change initApp into a hook, move app init hooks into new useInitApp

--- src/app/initApp.ts
+++ src/app/initApp.ts
@@ -10,6 +10,10 @@ import { appConfig } from 'config';
 import { initPushNotifications } from 'lib/notifications';
 // import storage from '@react-native-firebase/storage';
 import { svgImages } from 'theme';
+import { useAchievementConveyor } from 'lib/achievement';
+import { useChecklistActionScheduleUpdater } from 'lib/useChecklistActionScheduleUpdater';
+import { useDeviceShake } from 'lib/useDeviceShake';
+import { useUnknownPilot } from 'lib/pilot';
 
 export enum InitStatus {
   NotAuthorized = 'NotAuthorized',
@@ -17,40 +21,49 @@ export enum InitStatus {
   Success = 'Success',
 }
 
-export const initApp = async (): Promise<InitStatus> => {
-  try {
-    // Initialize firestore for dev as necessary.
-    if (__DEV__) {
-      // firestore().useEmulator('10.6.9.64', 8080);
-      // storage().useEmulator('10.6.9.64', 9199);
-      // console.log('Firestore emulator running at 10.6.9.100:8080');
-      // firestore().clearPersistence();
+export const useInitApp = () => {
+  useDeviceShake();
+
+  // Order is important here.
+  useUnknownPilot();
+  useChecklistActionScheduleUpdater();
+  useAchievementConveyor();
+
+  return async (): Promise<InitStatus> => {
+    try {
+      // Initialize firestore for dev as necessary.
+      if (__DEV__) {
+        // firestore().useEmulator('10.6.9.64', 8080);
+        // storage().useEmulator('10.6.9.64', 9199);
+        // console.log('Firestore emulator running at 10.6.9.100:8080');
+        // firestore().clearPersistence();
+      }
+
+      // Disable Android hardware back button.
+      BackHandler.addEventListener('hardwareBackPress', () => {
+        return true;
+      });
+
+      initPushNotifications();
+
+      AJPElements.init({
+        buildEnvironment: appConfig.buildEnvironment,
+        sentryEndpoint: appConfig.sentryEndpoint,
+        sentryLoggingEnabled: appConfig.sentryLoggingEnabled,
+        svgImages,
+        // userId: '',
+      });
+
+      GoogleSignin.configure({
+        webClientId: appConfig.firebaseOauthClientId,
+      });
+
+      return InitStatus.Success;
+
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    } catch (e: any) {
+      log.error(`App initialization: ${e.message}`);
+      throw new AppError(e.message);
     }
-
-    // Disable Android hardware back button.
-    BackHandler.addEventListener('hardwareBackPress', () => {
-      return true;
-    });
-
-    initPushNotifications();
-
-    AJPElements.init({
-      buildEnvironment: appConfig.buildEnvironment,
-      sentryEndpoint: appConfig.sentryEndpoint,
-      sentryLoggingEnabled: appConfig.sentryLoggingEnabled,
-      svgImages,
-      // userId: '',
-    });
-
-    GoogleSignin.configure({
-      webClientId: appConfig.firebaseOauthClientId,
-    });
-
-    return InitStatus.Success;
-
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  } catch (e: any) {
-    log.error(`App initialization: ${e.message}`);
-    throw new AppError(e.message);
-  }
+  };
 };

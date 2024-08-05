8c2da1bcfe4706b0561a4dc00e7e0378762178ec
Andy Phillipson <1063765+ajp8164@users.noreply.github.com>
2024-08-04 21:41:29 -0400

    Use static deck card back colors

--- src/components/molecules/card-deck/dinn/index.ts
+++ src/components/molecules/card-deck/dinn/index.ts
@@ -1,10 +1,12 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 import { DeckCardColors } from 'types/preferences';
+import { theme } from 'theme';
 
 export * from './Back';
 export * from './Front';
 
 export const defaultDinnCardColors: DeckCardColors = {
-  primary: '#999999',
-  accent1: '#474747',
-  accent2: '#707070',
+  primary: theme.lightColors!.lightGray!,
+  accent1: theme.lightColors!.darkGray!,
+  accent2: theme.lightColors!.midGray!,
 };

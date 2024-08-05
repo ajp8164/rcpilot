dba814b77dce26b082f9551f5a96193ed3ba633d
Andy Phillipson <1063765+ajp8164@users.noreply.github.com>
2024-08-04 21:11:21 -0400

    Create backdrop provider for disabling pointer events, implement on card properties modal

---
+++ src/components/atoms/Backdrop/BackdropProvider.tsx
@@ -0,0 +1,34 @@
+import { View } from 'react-native';
+import { BackdropContext } from './BackdropContext';
+import React, { ReactNode, useState } from 'react';
+import { makeStyles } from '@rneui/themed';
+import { AppTheme, useTheme } from 'theme';
+
+export const BackdropProvider = ({ children }: { children: ReactNode }): ReactNode => {
+  const theme = useTheme();
+  const s = useStyles(theme);
+
+  const [enabled, setEnabled] = useState(false);
+
+  return (
+    <BackdropContext.Provider
+      value={{
+        enabled,
+        setEnabled,
+      }}>
+      <View style={enabled ? s.enabled : s.disabled}>{children}</View>
+    </BackdropContext.Provider>
+  );
+};
+
+const useStyles = makeStyles((_theme, __theme: AppTheme) => ({
+  disabled: {
+    width: '100%',
+    height: '100%',
+  },
+  enabled: {
+    width: '100%',
+    height: '100%',
+    pointerEvents: 'none',
+  },
+}));

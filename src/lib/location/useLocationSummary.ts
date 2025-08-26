38a2fd15f2266b23ba8cd56a2387ce97507c2b37
Andy Phillipson <1063765+ajp8164@users.noreply.github.com>
2025-08-26 14:24:02 -0400

    Fix location summary for undefined location

--- src/lib/location/useLocationSummary.ts
+++ src/lib/location/useLocationSummary.ts
@@ -2,23 +2,32 @@ import { useRealm } from '@realm/react';
 import { DateTime } from 'luxon';
 import { Event, Location } from 'realmdb';
 
-export const useLocationSummary = (location: Location) => {
+export const useLocationSummary = (location?: Location) => {
+  console.log('ULS', location);
   const realm = useRealm();
-  const allEvents = realm
-    .objects(Event)
-    .filtered('location == $0', location)
-    .sorted('date');
 
-  const count = allEvents.length;
-  const countStr =
-    allEvents.length > 0
-      ? `${count} event${count !== 1 ? 's' : ''}, `
-      : 'No events';
+  let count = 0;
+  let countStr = '';
+  let date = '';
+  let dateStr = '';
 
-  const date = allEvents.length
-    ? DateTime.fromISO(allEvents[0].date).toFormat('M/d/yyyy')
-    : 'None';
-  const dateStr = allEvents.length > 0 ? `last on ${date}` : '';
+  if (location) {
+    const allEvents = realm
+      .objects(Event)
+      .filtered('location._id == $0', location._id)
+      .sorted('date');
+
+    count = allEvents.length;
+    countStr =
+      allEvents.length > 0
+        ? `${count} event${count !== 1 ? 's' : ''}, `
+        : 'No events';
+
+    date = allEvents.length
+      ? DateTime.fromISO(allEvents[0].date).toFormat('M/d/yyyy')
+      : 'None';
+    dateStr = allEvents.length > 0 ? `last on ${date}` : '';
+  }
 
   return {
     count,

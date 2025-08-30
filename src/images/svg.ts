71bb2c8ca6a4eefb210ebb643b0eefeae2ab708c
Andy Phillipson <1063765+ajp8164@users.noreply.github.com>
2025-08-30 18:07:24 -0400

    Image cleanup

--- src/images/svg.ts
+++ src/images/svg.ts
@@ -1,9 +1,5 @@
 import brandIcon from './img/brand/icon-brand.svg';
 import brandLogo from './img/brand/logo-brand.svg';
-import appleIcon from './img/icon-apple.svg';
-import facebookIcon from './img/icon-facebook.svg';
-import googleIcon from './img/icon-google.svg';
-import twitterIcon from './img/icon-twitter.svg';
 import airplane from './img/modelIcons/airplane.svg';
 import boat from './img/modelIcons/boat.svg';
 import car from './img/modelIcons/car.svg';
@@ -11,6 +7,12 @@ import helicopter from './img/modelIcons/helicopter.svg';
 import multicopter from './img/modelIcons/multicopter.svg';
 import robot from './img/modelIcons/robot.svg';
 import sailplane from './img/modelIcons/sailplane.svg';
+import appleIcon from './img/social/icon-apple.svg';
+import facebookIcon from './img/social/icon-facebook.svg';
+import googleIcon from './img/social/icon-google.svg';
+import twitterIcon from './img/social/icon-twitter.svg';
+import timer3 from './img/timer-3.svg';
+import timer30 from './img/timer-30.svg';
 
 export const svgImages = Object.assign(
   {},
@@ -29,5 +31,8 @@ export const svgImages = Object.assign(
     multicopter,
     robot,
     sailplane,
+
+    timer3,
+    timer30,
   },
 );
